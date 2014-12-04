4115bd138869987f3c46acfd9e5d538fbd17189a
NamelessOne <nlessone@gmail.com>
2014-12-04 17:44:40 +0300

    some fix

---
+++ consts.py
@@ -0,0 +1,7 @@
+_author__ = 'NamelessOne'
+import os
+
+HOST = os.environ['OPENSHIFT_MYSQL_DB_HOST']
+USER = os.environ['OPENSHIFT_MYSQL_DB_USERNAME']
+PASSWORD = os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']
+DB = os.environ['OPENSHIFT_APP_NAME']
