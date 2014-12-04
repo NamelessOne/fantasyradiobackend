0c47a8543f9ea22af6aaedfc9419655a3e78199b
NamelessOne <nlessone@gmail.com>
2014-12-04 15:39:14 +0400

    Another fix

--- sendNotification.py
+++ sendNotification.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'NamelessOne'
 import pymysql
+import consts
 
 
 def send_notification(userKey):
@@ -8,8 +9,42 @@ def send_notification(userKey):
 
 
 def get_all_entities_before_datetime(datetime):
-    pass
+    s = ""
+    try:
+        conn = pymysql.connect(host=consts.HOST, port=3306, user=consts.USER, passwd=consts.PASSWORD, db=consts.DB)
+    except Exception as e:
+        s = str(e)
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT CLIENT_KEY FROM ScheduleEntities WHERE date < %s", datetime)
+    except pymysql.DataError as e:
+        #Ошибки MySQL всегда четырехразрядные, помни об этом!!!
+        s = str(e.args[0])
+    except pymysql.IntegrityError as e:
+        s = str(e.args[0])
+    except pymysql.ProgrammingError as e:
+        s = str(e.args[0])
+    except pymysql.NotSupportedError as e:
+        s = str(e.args[0])
+    return cur
 
 
 def remove_all_entities_before_datetime(datetime):
-    pass
+    s = ""
+    try:
+        conn = pymysql.connect(host=consts.HOST, port=3306, user=consts.USER, passwd=consts.PASSWORD, db=consts.DB)
+    except Exception as e:
+        s = str(e)
+    cur = conn.cursor()
+    try:
+        cur.execute("DELETE FROM ScheduleEntities WHERE date < %s", datetime)
+    except pymysql.DataError as e:
+        #Ошибки MySQL всегда четырехразрядные, помни об этом!!!
+        s = str(e.args[0])
+    except pymysql.IntegrityError as e:
+        s = str(e.args[0])
+    except pymysql.ProgrammingError as e:
+        s = str(e.args[0])
+    except pymysql.NotSupportedError as e:
+        s = str(e.args[0])
+    return s
