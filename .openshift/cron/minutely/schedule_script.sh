dacdd2eebafb7607242dbf62b1c40928c11f6570
NamelessOne <nlessone@gmail.com>
2014-12-05 10:39:13 +0400

    Another fix

--- .openshift/cron/minutely/schedule_script.sh
+++ .openshift/cron/minutely/schedule_script.sh
@@ -1,2 +1,2 @@
-echo python $OPENSHIFT_REPO_DIR/sendNotification.py >> 2.txt
+echo python $OPENSHIFT_REPO_DIR/sendNotification.py >> $OPENSHIFT_REPO_DIR/2.txt
 echo "1111111" >> $OPENSHIFT_REPO_DIR/1.txt
