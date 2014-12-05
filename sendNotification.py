bd81450cee8f1ed04c44a338e759fb810aec259d
NamelessOne <nlessone@gmail.com>
2014-12-05 12:26:54 +0400

    Another fix

--- sendNotification.py
+++ sendNotification.py
@@ -21,7 +21,8 @@ def send_notification(user_keys):
     json_body = {"registration_ids": user_keys, "data": data}
     #-----------------------------
     r = Request(url=url, headers=headers, data=json.dumps(json_body))
-    print(r.read())
+    urllib.request.urlopen(r)
+    print(urllib)
     return
 
 
