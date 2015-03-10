bd1caba98e8e21721cbff1db9106e9e5347a9024
NamelessOne <nlessone@gmail.com>
2015-03-10 13:48:02 +0400

    Form table

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,7 @@ import sys
 import schedule
 import crashreports
 import templates_builder
+from http.cookies import SimpleCookie
 sys.path.append(os.path.dirname(__file__))
 
 
@@ -22,11 +23,15 @@ def application(environ, start_response):
     if environ['PATH_INFO'] == '/auth':
         start_response('200 OK', [('Content-Type', 'text/html')])
         return templates_builder.render('auth.html', 'text/html')
+    if environ['PATH_INFO'] == '/login':
+        #TODO залогиниваемся
+        environ['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return get_client_ip(environ)
     start_response('200 OK', [('Content-Type', 'text/html')])
     return ['''Hello %(subject)s
     #Hello %(subject)s!
-
-''' % {'subject': '111'}]
+    ''' % {'subject': '111'}]
 
 #
 # Below for testing only
@@ -37,3 +42,12 @@ if __name__ == '__main__':
     httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
     httpd.handle_reques()
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
