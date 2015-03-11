5b9a6498b63c11e1972716fef8c03b0ebfe9b5c1
NamelessOne <nlessone@gmail.com>
2015-03-11 15:33:53 +0400

    Form table

--- crashreports.py
+++ crashreports.py
@@ -97,8 +97,7 @@ def delete_report_by_id(report_id):
     try:
         conn = pymysql.connect(host=consts.HOST, port=3306, user=consts.USER, passwd=consts.PASSWORD, db=consts.DB)
         cur = conn.cursor()
-
-        raise Exception(cur.execute('DELETE FROM CrashReports WHERE REPORT_ID=%s', report_id))
+        cur.execute('DELETE FROM CrashReports WHERE REPORT_ID=\'%s\'', report_id)
         return
     except Exception:
         return
