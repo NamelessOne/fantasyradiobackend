c76cbce6c955536f5fb71122b2942c3ce8d7cb8f
NamelessOne <nlessone@gmail.com>
2015-07-11 15:17:43 +0300

    get schedule

--- schedule.py
+++ schedule.py
@@ -22,7 +22,7 @@ def get_schedule():
              'end': row['end'].isoformat(), 'img': row['img']}
         objects_list.append(d)
     print(json.dumps(objects_list, ensure_ascii=False))
-    return repr(json.dumps(objects_list, ensure_ascii=False)).encode('utf-8').decode()
+    return repr(json.dumps(objects_list, ensure_ascii=False)).encode('utf-8').decode('unicode')
 
 
 def _get_db_entities():
