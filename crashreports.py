ef542d11ef6dd0c663af1f6aca05fdb00df09382
NamelessOne <nlessone@gmail.com>
2016-03-19 11:46:56 +0500

    get schedule

--- crashreports.py
+++ crashreports.py
@@ -33,25 +33,26 @@ def add(environ):
                     "USER_COMMENT, USER_EMAIL, USER_APP_START_DATE, USER_CRASH_DATE, DUMPSYS_MEMINFO, LOGCAT, "
                     "INSTALLATION_ID, DEVICE_FEATURES, ENVIRONMENT, SHARED_PREFERENCES, SETTINGS_SYSTEM, "
                     "SETTINGS_SECURE) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, "
-                    "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (report.report_id.encode('utf-8'), report.app_version_code.encode('utf-8'),
-                                                                        report.app_version_name.encode('utf-8'), report.package_name.encode('utf-8'),
-                                                                        report.file_path.encode('utf-8'), report.phone_model.encode('utf-8'),
-                                                                        report.brand.encode('utf-8'),
-                                                                        report.product.encode('utf-8'), report.android_version.encode('utf-8'),
-                                                                        report.build.encode('utf-8'), report.total_mem_size.encode('utf-8'),
-                                                                        report.available_mem_size.encode('utf-8'), report.custom_data.encode('utf-8'),
-                                                                        report.is_silent.encode('utf-8'), report.stack_trace.encode('utf-8'),
-                                                                        report.initial_configuration.encode('utf-8'),
-                                                                        report.crash_configuration.encode('utf-8'), report.display.encode('utf-8'),
-                                                                        report.user_comment.encode('utf-8'),
-                                                                        report.user_email.encode('utf-8'), report.user_app_start_date.encode('utf-8'),
-                                                                        report.user_crash_date.encode('utf-8'), report.dumpsys_meminfo.encode('utf-8'),
-                                                                        report.logcat.encode('utf-8'), report.installation_id.encode('utf-8'),
-                                                                        report.device_features.encode('utf-8'), report.environment.encode('utf-8'),
-                                                                        report.shared_preferences.encode('utf-8'),
-                                                                        report.settings_system.encode('utf-8'), report.settings_secure.encode('utf-8')))
+                    "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (report.report_id.encode('utf-8'), report.app_version_code.encode('utf-8'),
+                     report.app_version_name.encode('utf-8'), report.package_name.encode('utf-8'),
+                     report.file_path.encode('utf-8'), report.phone_model.encode('utf-8'),
+                     report.brand.encode('utf-8'),
+                     report.product.encode('utf-8'), report.android_version.encode('utf-8'),
+                     report.build.encode('utf-8'), report.total_mem_size.encode('utf-8'),
+                     report.available_mem_size.encode('utf-8'), report.custom_data.encode('utf-8'),
+                     report.is_silent.encode('utf-8'), report.stack_trace.encode('utf-8'),
+                     report.initial_configuration.encode('utf-8'),
+                     report.crash_configuration.encode('utf-8'), report.display.encode('utf-8'),
+                     report.user_comment.encode('utf-8'),
+                     report.user_email.encode('utf-8'), report.user_app_start_date.encode('utf-8'),
+                     report.user_crash_date.encode('utf-8'), report.dumpsys_meminfo.encode('utf-8'),
+                     report.logcat.encode('utf-8'), report.installation_id.encode('utf-8'),
+                     report.device_features.encode('utf-8'), report.environment.encode('utf-8'),
+                     report.shared_preferences.encode('utf-8'),
+                     report.settings_system.encode('utf-8'), report.settings_secure.encode('utf-8')))
     except pymysql.DataError as e:
-        #Ошибки MySQL всегда четырехразрядные, помни об этом!!!
+        # Ошибки MySQL всегда четырехразрядные, помни об этом!!!
         s = str(e.args[0])
         response_body += "-" + s[1] + s[2] + s[3] + s[4]
         return response_body
@@ -87,7 +88,7 @@ def build_reports_table():
         rows = cur.fetchall()
         rows = [(elem + ("<a href=\"/delete?id=" + elem[0] + "\">Delete</a>", )) for elem in rows]
         result = __build_html_table(field_names, rows)
-        #------------------------
+        # ------------------------
         return result
     except Exception as e:
         return e
